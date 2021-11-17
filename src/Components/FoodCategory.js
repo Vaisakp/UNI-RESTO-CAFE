5abba84fb45629ad0bc6ff89c7f1de393a0f9b8f
vaisak <vaisakp07@gmail.com>
2021-11-17 12:53:27 +0530

    Item list completed

--- src/Components/FoodCategory.js
+++ src/Components/FoodCategory.js
@@ -1,31 +1,39 @@
 import React from 'react'
 
-const FoodCategory = () => {
+const FoodCategory = ({itemData}) => {
+//     addonCat: (2) [{…}, {…}]
+// dish_Availability: true
+// dish_Type: 2
+// dish_calories: 15
+// dish_currency: "SAR"
+// dish_description: "Fresh spinach, mushrooms, and hard-boiled egg served with warm bacon vinaigrette"
+// dish_id: "100000001"
+// dish_image: "https://i.imgur.com/PoJfqsD.jpg"
+// dish_name: "Spinach Salad"
+// dish_price: 7.95
+// nexturl: "http://snapittapp.snapitt.net/api/menu/30/?org=1010000001&branch_id=1000000001&menuItem=100000001&limit=10&offset=20&lang=en"
+// [[Prototype]]: Object
     return (
         <div className='flex items-center justify-between'>
-
             <div className=' flex flex-col'>
                     <div className='border border-green-500 p-1'>
                         <div className='bg-green-500 w-4 h-4 rounded-full '>
                         </div>
-                    </div>
-                
-                <div className='flex' >
-          
-                    <h1 className='font-semibold '>Spanish Salad</h1>
+                    </div>               
+                <div className='flex' >         
+                    <h1 className='font-semibold '>{itemData.dish_name}</h1>
                 </div>
-                <p className='font-medium my-2 '>25rs</p>
-                <p className='text-gray-400'>dfsavfsbaststesthh</p>
+                <p className='font-medium my-2 '>{itemData.dish_currency} {itemData.dish_price}</p>
+                <p className='text-gray-400'>{itemData.dish_description}</p>
                 <div className='bg-green-500 py-1 mt-4 rounded-3xl flex items-center justify-center text-white'>
                     <button className='font-bold px-4'>-</button>
                     <p className=' px-4'>0</p>
                     <button className='font-bold px-4'>+</button>
-
                 </div>
             </div>   
             <div className='flex items-center'>
-                <p className='font-semibold mr-8 '>15 calories</p>
-                <img className =' w-28 h-28 object-cover rounded-md' src="https://i.imgur.com/PoJfqsD.jpg" alt="imagesd" />
+                <p className='font-semibold mr-8 '>{itemData.dish_calories} calories</p>
+                <img className =' w-28 h-28 object-cover rounded-md' src={itemData.dish_image} alt="imagesd" />
             </div>
         </div>
     )
--- src/Components/Maincomponent.js
+++ src/Components/Maincomponent.js
@@ -1,30 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FoodCategory from './FoodCategory'
+import { data } from '../data'
 
 const Maincomponent = () => {
-    return (
-        <div className='flex mt-4 justify-center   flex-col overflow-x-scroll
-        ' >
+    const [categories, setcategories] = useState([])
+    const [Items, setItems] = useState([])
+    console.log(Items)
+    const getData=()=>{
+       setcategories(data[0].table_menu_list)
+    }
+    const changeItems =(category)=>{
+   
+        const filteredData = categories.filter((item=>item.menu_category === category))
+        setItems(filteredData[0].category_dishes)
+        
+    }
+    useEffect(() => {
+      getData();
+        
+    }, [])
 
+    return (
+        <div className='flex mt-4 justify-center   flex-col overflow-x-scroll' >
            <div className=' border-b flex 
         border-gray-400'>
-               <button className='text-red-500 p-4 border-b border-red-500 max-w-xl px-2 
+            { 
+               categories && categories.map((item)=>{return(
+                <button key={item.menu_category} className='text-red-500 p-4 border-b border-red-500 max-w-xl px-2 
                font-medium text-lg
-               '>Salad and soups</button>
-
-               
-               
-
-
+               ' onClick={()=>{changeItems(item.menu_category)}} >{item.menu_category}</button>
+               )})
+            }  
            </div>
-           <div className='mt-4 p-4 border-b 
+           { Items && Items.map((item)=>{
+               return(
+                    <div className='mt-4 p-4 border-b 
+                         border-gray-400' key={item.dish_id}>
+                         <FoodCategory itemData ={item}/>
+                    </div>
+               )
+           })
+
+           
+           }
+           {/* <div className='mt-4 p-4 border-b 
         border-gray-400'>
                <FoodCategory/>
-           </div>
-           <div className='mt-4 p-4 border-b 
-        border-gray-400'>
-               <FoodCategory/>
-           </div>
+           </div> */}
             {/* <div>
                <button>briyani</button>
                <FoodCategory/>
